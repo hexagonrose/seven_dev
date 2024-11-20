a0c4a5d809130d7a798b5dfe226a895d563f5b8f
hexagonrose <haekwan98@snu.ac.kr>
2024-11-20 16:53:56 +0900

    new benchmark

---
+++ calculator/Li20Ge2P4S24/run_supercell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --partition=loki1
+##SBATCH --nodelist=n008
+#SBATCH --job-name=benchmark
+#SBATCH --time=04-00:00              # Runtime limit: Day-HH:MM
+
+python make_supercell.py
