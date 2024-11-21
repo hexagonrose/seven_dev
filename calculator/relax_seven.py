87b40b5c5b2e467bec84752228b5652ae98d5567
hexagonrose <haekwan98@snu.ac.kr>
2024-11-21 10:47:02 +0900

    new benchmark run

--- calculator/relax_seven.py
+++ calculator/relax_seven.py
@@ -32,15 +32,15 @@ if __name__ == '__main__':
     end_time = time.time()
     print(f'Initiation done. Time : {end_time - start_time} s.')
 
-    cif_paths = glob(os.path.join(log_mat_path, f'{material}*.cif'))
+    cif_paths = glob(os.path.join(log_mat_path, f'structure/{material}*.cif'))
 
     for file_path in cif_paths:
         mat_full_name = os.path.basename(file_path).split('.')[0]
-        log_file = os.path.join(log_mat_path, f'{mat_full_name}.txt')
+        log_file = os.path.join(log_mat_path, f'{mat_full_name}_{run_type}.txt')
         write_log(log_file, f'7net-0 relaxation for {mat_full_name}.', mode='w')
         write_log(log_file, f'GPU: {torch.cuda.get_device_name(0)}')
         time_list = []
-        for i in range(0, 5):
+        for i in range(0, 21):
             # for idx in unique_id_list:
             atoms = read(file_path)
             write_log(log_file, f'Number of atoms: {len(atoms)}')
@@ -54,8 +54,8 @@ if __name__ == '__main__':
             # relax the structure
             relax = FIRE(ucf)
             start_time = time.time()
-            # relax.run(fmax=0.02, steps=3000)
-            relax.run(steps=100)
+            relax.run(fmax=0.05, steps=3000)
+            # relax.run(steps=200)
             end_time = time.time()
 
             # saved relaxed structure
@@ -67,4 +67,4 @@ if __name__ == '__main__':
             write_log(log_file, f'Relaxation done_{i}. Time : {end_time - start_time} s.')
             time_list.append(end_time - start_time)
 
-        write_log(log_file, f'Average time : {sum(time_list) / len(time_list)} s.')
+        write_log(log_file, f'Average time : {sum(time_list[1:]) / len(time_list[1:])} s.')
--- calculator/run_gpu_seven.sh
+++ calculator/run_gpu_seven.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --partition=gpu3
-#SBATCH --gres=gpu:1
-##SBATCH --nodelist=n008
+#SBATCH --partition=gpu
+##SBATCH --gres=gpu:1
+#SBATCH --nodelist=n008
 #SBATCH --job-name=benchmark_LGPS
 #SBATCH --time=04-00:00              # Runtime limit: Day-HH:MM
 
 # python3 compare_gnome_diffcsp.py
-# export CUDA_DEVICE_ORDER=PCI_BUS_ID
-# export CUDA_VISIBLE_DEVICES=1
+export CUDA_DEVICE_ORDER=PCI_BUS_ID
+export CUDA_VISIBLE_DEVICES=1
 python_path=/home/haekwan98/miniconda3/envs/seven_test/bin/python3
 code_path=/data2_1/haekwan98/sevennet_test/calculator
 
@@ -19,6 +19,6 @@ run_type='ase'
 
 for material in "${materials[@]}"; do
     # then relax unique structures
-    relax_log="$material"_seven_"$run_type"_avg.log
+    relax_log="$run_type"_avg.log
     $python_path relax_seven.py $material $run_type > $material/$relax_log
 done
